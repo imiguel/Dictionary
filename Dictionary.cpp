df83edfa66da07f4cb396a9cbe3cb0704bab9f8b
imiguel <miguel@Mac-mini.local>
2015-03-16 22:47:13 +0000

    - Words struct - function insert

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,1 +1,41 @@
 #include <stdlib.h>
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+#include "locale.h"
+
+
+//Constants
+#define MAX_WORD_SIZE 35 //max char size for each word
+
+
+/*
+Structs zone
+*/
+struct Words{
+	char lituanWord[MAX_WORD_SIZE];
+	char englishWord[MAX_WORD_SIZE];
+}
+
+
+/*
+Functions zone
+*/
+void insertWords(){
+	struct Words *insertedWord;
+	
+	printf("\nInsert Lithuan word (original): ");
+	scanf("%s", insertedWord->lituanWord);
+	printf("\nInsert english word (translated): ");
+	scanf("%s", insertedWord->englishWord);
+	
+	prinf("\n\nlithuan: %s", insertedWords->lithuanWord);
+}
+
+
+
+int main(int argc, char* argv[]){
+	struct Words *words;
+	
+	insertWords(&words);
+}
