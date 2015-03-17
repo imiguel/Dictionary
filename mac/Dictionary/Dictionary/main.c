abf3f311091a93ac2c65c5e1511d4f8b71dcc57d
imiguel <miguel@Mac-mini.local>
2015-03-17 13:17:03 +0000

    updates mac
    mac

---
+++ mac/Dictionary/Dictionary/main.c
@@ -0,0 +1,32 @@
+//
+//  main.c
+//  Dictionary
+//
+//  Created by Miguel Rosa on 17/03/15.
+//  Copyright (c) 2015 MR. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+//Constant
+#define MAX_WORD_SIZE 35 //max char size for each word
+
+
+/*
+    Structs zone
+ */
+struct Words{
+    char lithuanWord[MAX_WORD_SIZE];
+};
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
