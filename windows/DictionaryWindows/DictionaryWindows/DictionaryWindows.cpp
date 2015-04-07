9741adbe001d2e81ae4bbbc9086c62a761c92af9
imiguel <miguel.rosa17@gmail.com>
2015-04-07 11:07:57 +0100

    novos updates

--- windows/DictionaryWindows/DictionaryWindows/DictionaryWindows.cpp
+++ windows/DictionaryWindows/DictionaryWindows/DictionaryWindows.cpp
@@ -59,6 +59,105 @@ void insertWords(struct Word *word){
 	while ((ch = getchar()) != '\n' && ch != EOF);
 
 	fclose(fEnglish); //close the file
+
+	printf("\nWords inserted correctly.\n");
+	system("PAUSE");
+}
+
+void findWords(struct Word words[]){
+	FILE *fLithuan;
+	FILE *fEnglish;
+	char searchWord[MAX_WORD_SIZE];
+	int lineNumber = 0; //memorize the word line (lithuan)
+	int lineNumber2 = 0;
+	int ch;
+	char lithuanWord[MAX_WORD_SIZE];
+	char englishWord[MAX_WORD_SIZE];
+
+
+	printf("Insert the word to search (in lithuan): "); //ask a user for a word in lithuan
+	scanf("%35[^\n]s", searchWord);
+	while ((ch = getchar()) != '\n' && ch != EOF); //flush the input
+	printf("\n\n");
+
+	//============================= LITHUAN =============================
+	fLithuan = fopen("..\\Debug\\data\\lithuanDictionary.txt", "r");
+	char line[MAX_WORD_SIZE];
+
+	for (;;){
+		fgets(line, MAX_WORD_SIZE, fLithuan);
+		
+		if( !feof(fLithuan) ){
+			lineNumber++;
+		}else{
+			break;
+		}
+
+		sscanf(line, "%s", &lithuanWord);
+
+		if (strcmp(lithuanWord, searchWord) == 0) {
+			//printf("line number: %d\n", lineNumber); // what line 
+			printf("Lithuan: %s (original)\n", lithuanWord);
+			lineNumber2 = lineNumber;
+		}
+	}
+	fclose(fLithuan);
+
+
+
+	//============================= ENGLISH =============================
+	fEnglish = fopen("..\\Debug\\data\\englishDictionary.txt", "r");
+	char line2[MAX_WORD_SIZE];
+	int aux = 0;
+
+	for (;;){
+		fgets(line2, MAX_WORD_SIZE, fEnglish);
+
+		if (!feof(fEnglish)){
+			aux++;
+		}else{
+			break;
+		}
+
+		sscanf(line2, "%s", &englishWord);
+
+		if ( aux == lineNumber2){
+			//printf("English file line number: %d\n", lineNumber2); // what line
+			printf("English: %s (translated)\n", englishWord);
+		}
+
+	}
+
+	//close the file
+	fclose(fEnglish);
+	printf("\n\n\n");
+	system("PAUSE");
+}
+
+void sizeList(struct Word words[]){
+	FILE *fLithuan;
+	int numberOfRecords = 0;
+
+	fLithuan = fopen("..\\Debug\\data\\lithuanDictionary.txt", "r");
+	char line[MAX_WORD_SIZE];
+
+	for (;;){
+		fgets(line, MAX_WORD_SIZE, fLithuan);
+
+		if (!feof(fLithuan)){
+			numberOfRecords++;
+		}else{
+			break;
+		}
+
+		
+	}
+
+	printf("\nNumber of records (words): %d", numberOfRecords);
+
+	fclose(fLithuan);
+	printf("\n\n\n");
+	system("PAUSE");
 }
 
 
@@ -71,9 +170,10 @@ void mainMenu(){
 	printf("\n##     Dictionary Lithuan - English     ##");
 	printf("\n##                                      ##");
 	printf("\n##   Main menu                          ##");
-	printf("\n##    1. Insert words                   ##");
-	printf("\n##    2. Search words                   ##");
-	printf("\n##    3. Remove words                   ##");
+	printf("\n##    1. Insert words V                 ##");
+	printf("\n##    2. Search words V                 ##");
+	printf("\n##    3. Remove words X                 ##");
+	printf("\n##    4. Size list    V                 ##");
 	printf("\n##                                      ##");
 	printf("\n##    0. Exit                           ##");
 	printf("\n##########################################");
@@ -85,6 +185,32 @@ void graphicInsertWords(){
 	printf("\n##########################################\n\n");
 }
 
+void graphicSearchWords(){
+	printf("\n##########################################");
+	printf("\n##            SEARCH WORD               ##");
+	printf("\n##########################################\n\n");
+}
+
+void graphicSizeList(){
+	printf("\n##########################################");
+	printf("\n##             SIZE LIST                ##");
+	printf("\n##########################################\n\n");
+}
+
+void lastScreen(){
+	system("CLS");
+	printf("\n");
+	printf("\n##########################################");
+	printf("\n##     Dictionary Lithuan - English     ##");
+	printf("\n##                                      ##");
+	printf("\n##   Develop By: Ana Pontes             ##");
+	printf("\n##   NOME_UNIVERSIDADE                  ##");
+	printf("\n##   LOCALIDADE_DA_LITU                 ##");
+	printf("\n##   2015                               ##");
+	printf("\n##                                      ##");
+	printf("\n##########################################\n\n\n");
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -92,21 +218,49 @@ int main(int argc, char* argv[])
 	struct Word words;
 	setlocale(LC_ALL, "Portuguese");
 
+
 	//
-	do{
+	backToMenu:do{ //create a loop into menus. Só sai do loop quando se digita 0 (zero)
+		system("CLS");
 		mainMenu();
 		printf("\nInsert the option: ");
 		scanf("%d", &option);
 
-		if (option == 1){
-			while ((ch = getchar()) != '\n' && ch != EOF);
-			system("CLS");
-			graphicInsertWords()
-			insertWords(&words);
+		switch (option){
+			case 1:
+				system("CLS"); //clear the screen
+				graphicInsertWords(); //show the "graphics"
+				while ((ch = getchar()) != '\n' && ch != EOF); //clear de memory buffer
+				insertWords(&words); //call the function to insert words
+				goto backToMenu; //back to main menu
+				break;
+			case 2:
+				system("CLS"); //clear the screen
+				graphicSearchWords(); //show the "graphics"
+				while ((ch = getchar()) != '\n' && ch != EOF); //clear de memory buffer
+				findWords(&words); //call the function to search words
+				goto backToMenu; //back to main menu
+				break;
+			case 3:
+				goto backToMenu; //back to main menu
+				break;
+			case 4:
+				system("CLS"); //clear the screen
+				graphicSizeList(); //show the "graphics"
+				while ((ch = getchar()) != '\n' && ch != EOF); //clear de memory buffer
+				sizeList(&words); //call the function to search words
+				goto backToMenu; //back to main menu
+				break;
+			default:
+				break;
+		}
+
+		if (option == 0){ //exit program
+			lastScreen();
 			break;
 		}
 
-	} while (option == 0);
+	} while (true);
 
 	system("PAUSE");
 	return 0;
